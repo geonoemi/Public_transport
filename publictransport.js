802c84b50349783d8d6a704319852e9a5a195bdb
László Noémi <geonoemilaszlo@gmail.com>
2020-07-19 22:18:57 +0200

    schedule for trolleys with buttons

--- publictransport.js
+++ publictransport.js
@@ -2,15 +2,6 @@ $(document).ready(function(){
 
     let chosenVehicle="";
 
-    /*$('#vehicles').on('change',function(){ 
-        let isEmpty=$('.optionDiv');
-        if(isEmpty.children().length>0){
-            isEmpty.children().remove();
-        }
-        chosenVehicle=$('#vehicles :selected').text();
-        console.log('your chosen vehicle is: '+chosenVehicle);
-    });*/
-
     $('#tramIMG').on('click',function(){   
         $('#main').remove();
         appendTrams();
--- vehicles/trolleys.js
+++ vehicles/trolleys.js
@@ -5,23 +5,49 @@ let appendTrolleys=function(){
     let label= $('<label>').attr('class','labels').text('Choose trolley');  
     $('.optionDiv').append(label); 
     
-    let trolley = [
+    let buttonDiv = $('<div>').attr('id','buttonDiv');
+    $(trolleyDiv).append(buttonDiv);
+    /*let trolley = [
                 {val : 1, text: '8'},
                 {val : 2, text: '10'},
                 {val : 3, text: '19'}
                 ];    
     let selectTrolley = $('<select>').attr('id','selectTrolley').appendTo('.optionDiv');
-    $('.optionDiv').append(trolleyDiv);
     $(trolley).each(function() {
      selectTrolley.append($('<option>').attr('value',this.val).text(this.text));
-    });
+    });*/
+    let button8=$('<button>').attr('class','button').text('8');
+    let button10=$('<button>').attr('class','button').text('10');
+    let button19=$('<button>').attr('class','button').text('19');
 
+    $(buttonDiv).append(button8);
+    $(buttonDiv).append(button10);
+    $(buttonDiv).append(button19);
+
+    $('.optionDiv').append(trolleyDiv);
     chooseTrolley();
 };
 
 let chooseTrolley=function(){
 
-    $('#selectTrolley').on('change',function(){ 
+    $('.button').on('click', function(){
+        
+        emptyButton();
+
+        chosenTrolley=$(this).text();
+
+        if(chosenTrolley=='8'){
+            select8Station();
+        }
+        if(chosenTrolley=='10'){
+            select10Station();  
+        }
+        if(chosenTrolley=='19'){
+            select19Station();
+        }
+    });
+};
+   /* $('#selectTrolley').on('change',function(){ 
         let isEmpty=  $('#selectTrolley').next();
         if(isEmpty.children().length>0){
             isEmpty.children().remove();     
@@ -44,7 +70,7 @@ let chooseTrolley=function(){
             select19Station();
         }
     });    
-};
+};*/
 
 
 
